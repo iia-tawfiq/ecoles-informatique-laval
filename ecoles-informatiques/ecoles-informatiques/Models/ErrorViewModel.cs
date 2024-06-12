b7048c1938466af332cf3bb7244e0185d69344ba
TYrueouJeL <guerinremi.pro@gmail.com>
2024-06-12 11:12:42 +0200

    création du projet

---
+++ ecoles-informatiques/ecoles-informatiques/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ecoles_informatiques.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
