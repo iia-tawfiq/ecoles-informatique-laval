99b3a40d6d9805233b3807d0b7d277dc7b40a287
Nathan <nathanbillaud1@gmail.com>
2024-06-13 16:59:30 +0200

    création page diploma plus liste non fonctionnel de diplomes

---
+++ ecoles-informatiques/ecoles-informatiques/Controllers/DiplomaController.cs
@@ -0,0 +1,18 @@
+﻿using ecoles_informatiques.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace ecoles_informatiques.Controllers
+{
+    public class DiplomaController : Controller
+    {
+        public IActionResult Diploma()
+        {
+            return View();
+        }
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ecoles-informatiques/ecoles-informatiques/Models/DiplomaViewModel.cs
@@ -0,0 +1,9 @@
+﻿using ecoles_informatiques.Models;
+
+namespace ecoles_informatiques.Models
+{
+    public class DiplomaViewModel
+    {
+        public List<Diploma> Diplomas { get; set; }
+    }
+}
