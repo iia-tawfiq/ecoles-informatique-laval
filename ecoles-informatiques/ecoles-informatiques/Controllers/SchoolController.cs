f800919d053ce24edf767cbccfa0dd945d5a2ff7
Guiton1er <bahiergael@gmail.com>
2024-06-13 12:14:09 +0200

    trying to add new pages for schools

---
+++ ecoles-informatiques/ecoles-informatiques/Controllers/SchoolController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ecoles_informatiques.Data;
+using ecoles_informatiques.Models;
+using Microsoft.EntityFrameworkCore.Metadata.Conventions;
+
+namespace ecoles_informatiques.Controllers
+{
+    public class SchoolController : Controller
+    {
+        [Route("/ecoles")]
+        public IActionResult Index(int SchoolId = 1)
+        {
+            ApplicationDbContext model = new ApplicationDbContext();
+            School school = model.Schools.Find(SchoolId);
+
+            ViewData[nameof(SchoolId)] = SchoolId;
+
+            return View(model);
+        }
+    }
+}
