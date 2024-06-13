32d7faf5bfb0749efdb8a73ee6c26392b4db6890
Lucas <lucas.alleaume@iia-formation.fr>
2024-06-13 09:18:39 +0200

    Changing "Ville" into "City" Deleting wrong migration Creation of new migration "AddCityToSchool

--- ecoles-informatiques/ecoles-informatiques/Models/School.cs
+++ ecoles-informatiques/ecoles-informatiques/Models/School.cs
@@ -5,7 +5,7 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public string Address { get; set; }
-        public string Ville { get; set; }
+        public string City { get; set; }
 
         public string Description { get; set; }
         public string Slug { get; set; }
