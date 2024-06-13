8848c16332a8c9a0daea0beea1242e9b7c3d5e47
Lucas <lucas.alleaume@iia-formation.fr>
2024-06-13 09:08:03 +0200

    Add "Ville" to School

--- ecoles-informatiques/ecoles-informatiques/Models/School.cs
+++ ecoles-informatiques/ecoles-informatiques/Models/School.cs
@@ -5,6 +5,8 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public string Address { get; set; }
+        public string Ville { get; set; }
+
         public string Description { get; set; }
         public string Slug { get; set; }
 
