bcb6611daf82d24d27e103f2acde8614c6d23dfa
Aizon <killian.bonneau@iia-formation.fr>
2024-06-13 12:14:03 +0200

    WIP - Working on school search by filter

--- ecoles-informatiques/ecoles-informatiques/Controllers/SchoolController.cs
+++ ecoles-informatiques/ecoles-informatiques/Controllers/SchoolController.cs
@@ -2,6 +2,7 @@ using ecoles_informatiques.Data;
 using ecoles_informatiques.Models;
 using ecoles_informatiques.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualBasic;
 
 namespace ecoles_informatiques.Controllers;
 
@@ -16,9 +17,22 @@ public class SchoolController : Controller
 
     [HttpGet]
     [Route("/ecoles")]
-    public IActionResult Index()
+    public IActionResult Index(string? search)
     {
-        SchoolEnumerationViewModel model = new SchoolEnumerationViewModel(new List<School>());
-        return View(model);
+        List<School> schools = _context.Schools.ToList();
+
+        search = Strings.Trim(search).ToLower();
+        List<School> filteredSchools;
+
+        if (!string.IsNullOrEmpty(search))
+        {
+            filteredSchools = schools.Where(s => s.Name.ToLower().Contains(search)).ToList();
+        }
+        else
+        {
+            filteredSchools = schools;
+        }
+        
+        return View(new SchoolEnumerationViewModel(filteredSchools));
     }
 }
