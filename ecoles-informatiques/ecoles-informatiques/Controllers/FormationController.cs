c18f518b87d3f64d6418bf58b37fa8875d22db2f
Lucas <lucas.alleaume@iia-formation.fr>
2024-06-13 12:14:00 +0200

    Work in progress

---
+++ ecoles-informatiques/ecoles-informatiques/Controllers/FormationController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ecoles_informatiques.Controllers
+{
+    public class FormationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
