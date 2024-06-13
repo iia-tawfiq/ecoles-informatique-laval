e14f4a8f04f0c4688c20b733816ab1a6fe6e6430
Guiton1er <bahiergael@gmail.com>
2024-06-13 16:56:50 +0200

    Work in progress - add pages per school

--- ecoles-informatiques/ecoles-informatiques/Controllers/SchoolController.cs
+++ ecoles-informatiques/ecoles-informatiques/Controllers/SchoolController.cs
@@ -2,20 +2,25 @@
 using ecoles_informatiques.Data;
 using ecoles_informatiques.Models;
 using Microsoft.EntityFrameworkCore.Metadata.Conventions;
+using Microsoft.EntityFrameworkCore;
 
-namespace ecoles_informatiques.Controllers
+namespace ecoles_informatiques.Controllers;
+
+public class SchoolController : Controller
 {
-    public class SchoolController : Controller
+    private ApplicationDbContext _context;
+
+    public SchoolController(ApplicationDbContext context)
     {
-        [Route("/ecoles")]
-        public IActionResult Index(int SchoolId = 1)
-        {
-            ApplicationDbContext model = new ApplicationDbContext();
-            School school = model.Schools.Find(SchoolId);
+        _context = context;
+    }
 
-            ViewData[nameof(SchoolId)] = SchoolId;
+    [Route("/ecole/")]
+    public IActionResult School(int schoolId)
+    {
+        schoolId = 2;
+        School school = _context.Schools.Find(schoolId);
 
-            return View(model);
-        }
+        return View(school);
     }
 }
