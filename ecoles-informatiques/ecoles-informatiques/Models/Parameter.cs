2f9597cb302e88fdf248f4db6e0846c8d12e0870
Lucas <lucas.alleaume@iia-formation.fr>
2024-06-13 16:42:44 +0200

    Création des parameters

---
+++ ecoles-informatiques/ecoles-informatiques/Models/Parameter.cs
@@ -0,0 +1,14 @@
+﻿namespace ecoles_informatiques.Models
+{
+    public class Parameter
+    {
+        public int Id { get; set; }
+
+        public string Label { get; set; }
+
+        public string Text { get; set; }
+
+        public string KeyValue { get; set; }
+
+    }
+}
