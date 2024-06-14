1f4fd668d4d11e416b7f63b20c0125110f45a33b
Tawfiq <tawfiq@caditazi.fr>
2024-06-14 14:04:12 +0200

    Location seeds

---
+++ ecoles-informatiques/ecoles-informatiques/Data/Migrations/20240614120254_SeedsLocation.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using NetTopologySuite.Geometries;
+
+#nullable disable
+
+namespace ecoles_informatiques.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedsLocation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Point>(
+                name: "Location",
+                table: "Schools",
+                type: "geography",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Schools",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Location",
+                value: (NetTopologySuite.Geometries.Point)new NetTopologySuite.IO.WKTReader().Read("SRID=4326;POINT (-0.769905 48.070711)"));
+
+            migrationBuilder.UpdateData(
+                table: "Schools",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Location",
+                value: (NetTopologySuite.Geometries.Point)new NetTopologySuite.IO.WKTReader().Read("SRID=4326;POINT (-0.780618 48.070058)"));
+
+            migrationBuilder.UpdateData(
+                table: "Schools",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Location",
+                value: (NetTopologySuite.Geometries.Point)new NetTopologySuite.IO.WKTReader().Read("SRID=4326;POINT (-0.770441 48.071255)"));
+
+            migrationBuilder.UpdateData(
+                table: "Schools",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "Location",
+                value: (NetTopologySuite.Geometries.Point)new NetTopologySuite.IO.WKTReader().Read("SRID=4326;POINT (-0.769948 48.068339)"));
+
+            migrationBuilder.UpdateData(
+                table: "Schools",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "Location",
+                value: (NetTopologySuite.Geometries.Point)new NetTopologySuite.IO.WKTReader().Read("SRID=4326;POINT (-0.770282 48.069778)"));
+
+            migrationBuilder.UpdateData(
+                table: "Schools",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "Location",
+                value: (NetTopologySuite.Geometries.Point)new NetTopologySuite.IO.WKTReader().Read("SRID=4326;POINT (-0.800985 48.061545)"));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Location",
+                table: "Schools");
+        }
+    }
+}
