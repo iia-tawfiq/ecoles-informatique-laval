7d7befde267d171b807976e72df56464eea2e179
Lucas <lucas.alleaume@iia-formation.fr>
2024-06-14 10:32:33 +0200

    Controller & Index creation

---
+++ ecoles-informatiques/ecoles-informatiques/Controllers/FormationController.cs
@@ -0,0 +1,21 @@
+﻿using ecoles_informatiques.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ecoles_informatiques.Controllers
+{
+    public class FormationController : Controller
+    {
+        private ApplicationDbContext _context;
+
+
+        public FormationController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index(string Name)
+        {
+
+            return View();
+        }
+    }
+}
