c74243f92cf8af20792d3cbf5a32ce8369701145
Lucas <lucas.alleaume@iia-formation.fr>
2024-06-14 09:53:19 +0200

    Adding Parameter table + fix PasswordHash

---
+++ ecoles-informatiques/ecoles-informatiques/Data/Migrations/20240614075229_AddParameterTable.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ecoles_informatiques.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddParameterTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
