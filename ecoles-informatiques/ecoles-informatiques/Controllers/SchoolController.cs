f56f6733f19f5dd3fd1c726b12b5cff0d6b0d474
Guiton1er <bahiergael@gmail.com>
2024-06-14 12:26:19 +0200

    merging

--- ecoles-informatiques/ecoles-informatiques/Controllers/SchoolController.cs
+++ ecoles-informatiques/ecoles-informatiques/Controllers/SchoolController.cs
@@ -1,13 +1,9 @@
+using Microsoft.AspNetCore.Mvc;
 using ecoles_informatiques.Data;
 using ecoles_informatiques.Models;
-using ecoles_informatiques.Models.ViewModels;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.VisualBasic;
 
-namespace ecoles_informatiques.Controllers;
-
-public class SchoolController : Controller
-{
+    public class SchoolController : Controller
+    {
     private readonly ApplicationDbContext _context;
 
     public SchoolController(ApplicationDbContext context)
@@ -16,22 +12,10 @@ public class SchoolController : Controller
     }
 
     [HttpGet]
-    [Route("/ecoles")]
-    public IActionResult Index(string? search)
-    {
-        ViewData["currentFilter"] = search;
-        List<School> schools = _context.Schools.ToList();
-
-        search = Strings.Trim(search).ToLower();
-        List<School> filteredSchools;
-
-        if (!string.IsNullOrEmpty(search))
+        [Route("/ecoles")]
         {
-            filteredSchools = schools.Where(s => s.Name.ToLower().Contains(search)).ToList();
-        }
-        else
-        {
-            filteredSchools = schools;
+
+
         }
         
         return View(new SchoolEnumerationViewModel(filteredSchools));
