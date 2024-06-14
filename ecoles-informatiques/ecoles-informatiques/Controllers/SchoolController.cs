3d986bc5d2e2ccb3bd724050696c36a1f15691cd
Guiton1er <bahiergael@gmail.com>
2024-06-14 12:26:19 +0200

    merging

--- ecoles-informatiques/ecoles-informatiques/Controllers/SchoolController.cs
+++ ecoles-informatiques/ecoles-informatiques/Controllers/SchoolController.cs
@@ -6,21 +6,10 @@ using Microsoft.EntityFrameworkCore;
 
 namespace ecoles_informatiques.Controllers;
 
-public class SchoolController : Controller
-{
-    private ApplicationDbContext _context;
-
-    public SchoolController(ApplicationDbContext context)
+    public class SchoolController : Controller
     {
-        _context = context;
-    }
+        {
 
-    [HttpGet]
-    [Route("/ecole/{schoolId}")]
-    public IActionResult School(int schoolId)
-    {
-        School school = _context.Schools.Find(schoolId);
 
-        return View(school);
     }
 }
