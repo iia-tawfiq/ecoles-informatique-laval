c5730b6e09342f6eb53960d6beaacaaf5b6a90a9
Guiton1er <bahiergael@gmail.com>
2024-06-14 12:26:38 +0200

    correcting conflicts

--- ecoles-informatiques/ecoles-informatiques/Controllers/SchoolController.cs
+++ ecoles-informatiques/ecoles-informatiques/Controllers/SchoolController.cs
@@ -6,10 +6,21 @@ using Microsoft.EntityFrameworkCore;
 
 namespace ecoles_informatiques.Controllers;
 
-    public class SchoolController : Controller
+public class SchoolController : Controller
+{
+    private ApplicationDbContext _context;
+
+    public SchoolController(ApplicationDbContext context)
     {
-        {
+        _context = context;
+    }
 
+    [HttpGet]
+    [Route("/ecole/{schoolId}")]
+    public IActionResult School(int schoolId)
+    {
+        School school = _context.Schools.Find(schoolId);
 
+        return View(school);
     }
 }
